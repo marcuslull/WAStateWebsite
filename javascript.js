700538544db297841ed125be991bc32e1ca55ebc
Marcus Lull <marcuslull@gmail.com>
2023-06-29 13:44:13 -0700

    fixing realtime status on confirm email matching

--- javascript.js
+++ javascript.js
@@ -3,13 +3,20 @@
 let validEmail = false;
 let emailWidget = document.querySelector("#emailInput");
 let confirmEmailWidget = document.querySelector("#confirmEmailInput");
+let confirmEmailLabel = document.querySelector("#confirmEmailLabel");
 confirmEmailWidget.addEventListener("input", checkEmails);
 
 
 function checkEmails() {
     let email = emailWidget.value;
     let confirmEmail = confirmEmailWidget.value;
-    validEmail = email === confirmEmail;
+    validEmail = email.includes(confirmEmail);
+    if (!validEmail) {
+        confirmEmailLabel.innerHTML = "confirm email <span id='emailMatchMessage'>***emails must match!***</span>";
+    }
+    else {
+        confirmEmailLabel.innerHTML = "confirm email"
+    }
 }
 
 let formWidget = document.querySelector("#infoForm");
